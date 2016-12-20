74f168c34d839a788c134d8a6f85237f5d10b2da
itecnis <romancastellarin@gmail.com>
2016-12-20 11:06:20 -0300

    final step

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,6 +50,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
 ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
